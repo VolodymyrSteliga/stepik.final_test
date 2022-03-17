7e55ac28537d86fecbf64e5841bcdb8ee6f8e4bc
VSteliga <89745238+VolodymyrSteliga@users.noreply.github.com>
2022-03-17 14:12:49 +0200

    test

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
-from pages.base_page import BasePage
 from pages.main_page import MainPage
+from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 
@@ -7,8 +7,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
-
-
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(
@@ -16,13 +14,8 @@ def test_guest_can_go_to_login_page(browser):
     )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-
-
-"""
-def test_add_to_cart(browser):
-    page = ProductPage(url="", browser)   # инициализируем объект Page Object
-    page.open()                           # открываем страницу в браузере
-    page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
-    page.add_product_to_cart()            # жмем кнопку добавить в корзину 
-    page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
-"""
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
